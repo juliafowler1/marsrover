3e58f8d1256eddc101a2906d8c418834ef0d8ed6
Julia Fowler <juliafowler@live.com>
2019-04-03 21:01:46 -0400

    first commit

---
+++ app/gridcontainer/gridcontainer.component.js
@@ -0,0 +1,17 @@
+const gridContainer = {
+    templateUrl:"app/gridcontainer/gridcontainer.template.html",
+    controller: [function(){
+        const vm = this;
+        
+
+    }]
+
+
+
+}
+
+
+
+
+
+angular.module("app").component("gridContainer", gridContainer);
---
+++ app/griditem/griditem.component.js
@@ -0,0 +1,8 @@
+const gridItem = {
+    templateUrl: "app/griditem/griditem.template.html",
+    controller: [function(){
+        const vm = this;
+    }]
+}
+
+angular.module("app").component("gridItem", gridItem)
