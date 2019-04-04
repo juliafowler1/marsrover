3bd2dba51270cef991f58d2fd1fec28f3222086d
Julia Fowler <juliafowler@live.com>
2019-04-03 22:00:27 -0400

    added bindings, bootstrap, set up basic grid

--- app/gridcontainer/gridcontainer.component.js
+++ app/gridcontainer/gridcontainer.component.js
@@ -1,12 +1,19 @@
+const GRIDSIZE = 4;
 const gridContainer = {
     templateUrl:"app/gridcontainer/gridcontainer.template.html",
     controller: [function(){
-        const vm = this;
         
+        const vm = this;
+        vm.currentX = 1;
+        vm.currentY = 2;
+        vm.coordinates = []
+        for (let i = 0; i < GRIDSIZE; i++) {
+            for (let j = 0; j < GRIDSIZE; j++) {
+                vm.coordinates.push({x: i, y: j})
+            }
+        }
 
     }]
-
-
 
 }
 
--- app/griditem/griditem.component.js
+++ app/griditem/griditem.component.js
@@ -2,7 +2,12 @@ const gridItem = {
     templateUrl: "app/griditem/griditem.template.html",
     controller: [function(){
         const vm = this;
-    }]
+    }],
+    bindings: {
+        x: "<",
+        currentX: "<",
+        selected: "<"
+    }
 }
 
 angular.module("app").component("gridItem", gridItem)
